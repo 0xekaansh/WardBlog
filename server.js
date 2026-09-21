d8367127123f248d4c27c37d89278fffeef57e29
Ekaansh Gautam <ekaansh2304@gmail.com>
2026-09-21 16:55:09 +0530

    add and edit post feature added

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import axios from "axios";
 import dotenv, { config } from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -8,11 +13,11 @@ const app = express();
 const port = process.env.SERVER_PORT || 3000;
 const API_URL = process.env.API_URL;
 
-app.use(express.static("public"));
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//homepage
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(`${API_URL}/posts`);
@@ -22,7 +27,51 @@ app.get("/", async (req, res) => {
   }
 });
 
+//route to createPost-page
+app.get("/new", (req, res) => {
+  res.render("createPost.ejs", { heading: "New Post", submit: "Create Post" });
+});
+
+//newPost-submission
+app.post("/api/blog", async (req, res) => {
+  try {
+    const response = await axios.post(`${API_URL}/posts`, req.body);
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error creating post" });
+  }
+});
+
+//oldPost-edit-get
+app.get("/edit/:id", async (req, res) => {
+  try {
+    const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+    console.log(response.data);
+    res.render("createPost.ejs", {
+      heading: "Edit Post",
+      submit: "Update Post",
+      post: response.data,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching Post" });
+  }
+});
+
+//oldPost-edit-post
+app.post("/api/posts/:id", async (req, res) => {
+  try {
+    const response = await axios.patch(
+      `${API_URL}/posts/${req.params.id}`,
+      req.body,
+    );
+    console.log(response.data);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: "Error updating post" });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Backend server is running on http://localhost:${port}`);
-  });
+  console.log(`Backend server is running on http://localhost:${port}`);
+});
